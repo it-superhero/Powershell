290f049739f6287876330d39f794353556c20432
it-superhero <162280626+it-superhero@users.noreply.github.com>
2024-03-05 08:13:56 +0200

    Add files via upload
    Uninstall all default Appx Packages.

---
+++ Uninstall AppXPackages.ps1
@@ -0,0 +1,7 @@
+﻿# Get all installed AppX package full names
+$packages = Get-AppxPackage | Select-Object -ExpandProperty PackageFullName
+
+# Loop through the list and remove each package
+foreach ($package in $packages) {
+    Remove-AppxPackage -Package $package
+}
